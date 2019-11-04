da4da1787eef6a342890530ee29c886307914554
Dmitry Verteyko <verteyko1990@gmail.com>
2019-11-04 19:45:54 +0200

    header

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,20 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <HeaderTemplate/>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue';
+import HeaderTemplate from './components/HeaderTemplate.vue';
 
 export default {
   name: 'app',
   components: {
-    HelloWorld,
+    HeaderTemplate,
   },
 };
 </script>
 
 <style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+
 </style>
---
+++ src/components/HeaderTemplate.vue
@@ -0,0 +1,27 @@
+<template lang="pug">
+  .lp-header
+    .lp-contanier
+      .lp-header-row
+
+</template>
+
+<script>
+export default {
+  name: 'NavigationTemplate',
+}
+</script>
+
+<style scoped lang="scss">
+  @import '../assets/scss/core.scss';
+  .lp-header {
+    width: 100%;
+    background: url(../assets/images/headerbg.jpg)no-repeat;
+    background-size: 100%;
+    min-height: 100vh;
+    height: 700px;
+    &-row {
+      display: flex;
+      justify-content: flex-start;
+    }
+  }
+</style>
