4b7ff80459408beb79b3a1e5d2f817a0f1e829fd
Dmitry Verteyko <verteyko1990@gmail.com>
2019-11-05 23:31:56 +0200

    gift

--- src/App.vue
+++ src/App.vue
@@ -2,18 +2,21 @@
   <div id="app">
     <HeaderTemplate/>
     <GiftTemplate/>
+    <GoodsTemplate/>
   </div>
 </template>
 
 <script>
 import HeaderTemplate from './components/HeaderTemplate.vue';
 import GiftTemplate from './components/GiftTemplate.vue';
+import GoodsTemplate from './components/GoodsTemplate.vue';
 
 export default {
   name: 'app',
   components: {
     HeaderTemplate,
     GiftTemplate,
+      GoodsTemplate,
   },
 };
 </script>
---
+++ src/components/GoodsTemplate.vue
@@ -0,0 +1,48 @@
+<template>
+    <div class="lp-goods">
+        <div class="lp-container">
+            <h2 class="lp__title lp-goods__title">Каталог детских гироскутеров</h2>
+            <div class="lp-goods-row">
+                <div class="lp-goods-row__item"></div>
+                <div class="lp-goods-row__item"></div>
+                <div class="lp-goods-row__item"></div>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+    import Slick from 'vue-slick';
+    export default {
+        name: "GoodsTemplate",
+        components: {
+            Slick
+        },
+    }
+</script>
+
+<style scoped lang="scss">
+    @import '../assets/scss/core.scss';
+    import 'node_modules/slick-carousel/slick/slick.css';
+
+    .lp-goods {
+        width: 100%;
+        &__title {
+            padding-top: 90px;
+        }
+        &-row {
+            display: flex;
+            flex-direction: row;
+            justify-content: flex-start;
+            padding-top: 60px;
+            &__item {
+                border: 1px solid red;
+                width: 33%;
+                height: 600px;
+                & + & {
+                    margin-left: 10px;
+                }
+            }
+        }
+    }
+</style>
