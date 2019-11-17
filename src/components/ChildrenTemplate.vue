50026a63f6e00950e9d85e9ae1374140459bbd0c
Dmitry Verteyko <verteyko1990@gmail.com>
2019-11-17 12:51:27 +0200

    responsive sections

--- src/components/ChildrenTemplate.vue
+++ src/components/ChildrenTemplate.vue
@@ -24,17 +24,52 @@
         background: url(../assets/images/reviewbg.png)no-repeat;
         min-height: 350px;
         padding-top: 75px;
+        background-size: cover;
+        background-position: center;
+        @media screen and (max-width: 1199px) {
+            background: url(../assets/images/children-bg-mobile.png)no-repeat;
+        }
+        @media screen and (max-width: 700px) {
+            padding-bottom: 50px;
+        }
     &-block {
          max-width: 700px;
          display: flex;
          margin-right: auto;
          flex-direction: column;
+        @media screen and (max-width: 1199px) {
+            justify-content: center;
+            margin: 0 auto;
+        }
+        @media screen and (max-width: 768px) {
+            & {
+                max-width: 640px;
+            }
+        }
+        @media screen and (max-width: 700px) {
+            & {
+                max-width: 400px;
+                text-align: center;
+            }
+        }
     &__title {
          font-size: 40px;
          line-height: 46px;
          color: $subtitlesColor;
          font-family: 'Open Sans',sans-serif;
          font-weight: 100;
+        @media screen and (max-width: 700px) {
+            & {
+                font-size: 30px;
+                line-height: 32px;
+            }
+        }
+        @media screen and (max-width: 419px) {
+            & {
+                font-size: 20px;
+                line-height: 22px;
+            }
+        }
      }
     &__desc {
          font-family: 'Open Sans Bold',sans-serif;
@@ -49,6 +84,12 @@
          color: $mainTitleColor;
          padding-right: 10px;
          font-weight: bold;
+        @media screen and (max-width: 419px) {
+            & {
+                font-size: 20px;
+                line-height: 22px;
+            }
+        }
      }
     }
     &-text {
@@ -68,11 +109,45 @@
     }
     }
     &-form {
+        @media screen and (max-width: 374px) {
+            & {
+                display: flex;
+                flex-direction: column;
+                justify-content: center;
+                align-items: center;
+            }
+        }
     &__input {
          height: 48px;
+        @media screen and (max-width: 419px) {
+            & {
+                min-width: auto;
+            }
+        }
+        @media screen and (max-width: 374px) {
+            & {
+                min-width: 300px;
+                margin-left: 0;
+
+            }
+            & + & {
+                margin-top: 20px;
+            }
+        }
      }
     &__button {
          height: 48px;
+        @media screen and (max-width: 700px) {
+            & {
+                margin-top: 30px;
+            }
+        }
+        @media screen and (max-width: 374px) {
+            & {
+                margin-left: 0;
+                margin-top: 10px;
+            }
+        }
      }
     }
     }
