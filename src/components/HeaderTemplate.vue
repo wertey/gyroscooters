f34586b1cd6d0a3177a8621c2b7e3bce70bd2ab5
Dmitry Verteyko <verteyko1990@gmail.com>
2019-11-22 22:21:17 +0200

    counter

--- src/components/HeaderTemplate.vue
+++ src/components/HeaderTemplate.vue
@@ -16,19 +16,7 @@
         span.lp-header__title_bold Распродажа
         span.lp-header__title_light детских гироскутеров!
       h2.lp-header__desc До завершения распродажи осталось:
-      VueCountdownTimer( @start_callback="startCallBack('event started')"
-          @end_callback="endCallBack('event ended')"
-          :start-time="'2018-10-10 00:00:00'"
-          :end-time="1481450115"
-          :interval="1000"
-          :start-label="'Until start:'"
-          :end-label="'Until end:'"
-          label-position="begin"
-          :end-text="'Event ended!'"
-          :day-txt="'days'"
-          :hour-txt="'hours'"
-          :minutes-txt="'minutes'"
-          :seconds-txt="'seconds'")
+      timerTemplate(starttime='Nov 5, 2019 15:37:25', endtime='Nov 28, 2019 16:37:25')
       .lp-header-main
         .lp-header-main-advantages
           .lp-header-main-advantages__item Бесплатная доставка в течении 3-х часов
@@ -58,12 +46,12 @@
 </template>
 
 <script>
-    import VueCountdownTimer from 'vuejs-countdown-timer'
+    import timerTemplate from '../components/timerTemplate.vue'
 
 export default {
   name: 'NavigationTemplate',
   components: {
-      VueCountdownTimer,
+      timerTemplate,
   },
     data() {
       return {
